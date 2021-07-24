bbb9a3cd64d4e5030719bcc5a1249fdc4ca9179b
plerros <48536508+plerros@users.noreply.github.com>
2021-07-24 18:25:42 +0300

    code cleanup
    -Adopt kernel code style
    -MINIMUM can now be set to 0
    -Add error handling for MIN > MAX
    -Switch to unsigned long to support the entire digit range
    -Replaced messy "optimized" code with a simple and clean implementation.
    
    All the above without any measurable performance loss.

--- milrab.c
+++ milrab.c
@@ -4,138 +4,129 @@
 #include <time.h>
 #include <limits.h>
 
-//====================================================+ DEFINITIONS +====================================================
-#define MINIMUM	3990000000
-#define MAXIMUM 4010000000
-//		4294967295
-#define	ROOF	4759123141
+#define MINIMUM 3990000000UL	// UL == unsigned long
+#define MAXIMUM 4010000000UL
+#define ROOF    4759123141UL
+
 //======================================================+ MODULO  +======================================================
-unsigned int modulo(unsigned long p, unsigned int k, unsigned int m){
-if(k == 0)	{return 1;				}
-else if(k & 1)	{return((p * modulo(p,k-1,m)) % m);	}
-else		{return(modulo((p*p) % m, k >> 1, m));	}
+static unsigned long modulo(unsigned long p, unsigned long k, unsigned long m)
+{
+	if (k == 0)
+		return 1;
+	else if (k % 2 == 1)
+		return((p * modulo(p, k-1, m)) % m);
+	else
+		return(modulo((p*p) % m, k/2, m));
 }
+
 //===================================================+ DETERMINISTIC +===================================================
-unsigned int deterministic_calc(unsigned int current){
-unsigned int i;
-	for(i = 5; i * i <= current; i += 4){
-		if(current % i == 0){return 0;}
-		i += 2;		if(i * i > current){return 1;}
-		else if(current % i == 0){return 0;}
+static unsigned long deterministic_calc(unsigned long current)
+{
+	for (unsigned long i = 5; i * i <= current; i += 4) {
+		if (current % i == 0)
+			return 0;
+		i += 2;
+		if (i * i > current)
+			return 1;
+		else if (current % i == 0)
+			return 0;
 	}
 	return 1;
 }
-//-----------------------------------------------------------------------------------------------------------------------
-void deterministic(unsigned int current, unsigned int range){
-	clock_t time = clock();
-	unsigned int counter = 0;
-	unsigned int i;
 
-	printf("Deterministic algorithm: Found ");
+static void deterministic(unsigned long current, unsigned long range)
+{
+	clock_t start = clock();
+	unsigned long counter = 0;
 
-	if(current == 1){current++;}
-	if(current == 2){current++;	counter++;}
-	if(current == 3){current += 2;	counter++;}
+	for (; current < range; current++) {
+		if (current == 1)
+			continue;
 
-	if(current % 2 == 0 && current + 1 < range){current++;}
+		if (current == 2)
+			counter++;
+		if (current % 2 == 0)
+			continue;
 
-	for(;(current + 1) % 6 != 0 && current + 2 <= range;current += 2){	//"current" normalizer to the expected
-		if(current % 3 == 0){continue;}					//values {5,11,17,23,29,..... (6*n)-1}
+		if (current == 3)
+			counter++;
+		if (current % 3 == 0)
+			continue;
+		
 		counter += deterministic_calc(current);
 	}
-	for(; current + 2 <= range; current += 4){				//{2,4}step prime scanner
+	printf("Deterministic algorithm: Found %d primes", counter);
 
-		counter += deterministic_calc(current);
-		current += 2;
-		counter += deterministic_calc(current);
-	}
-	if(current < range){
-		counter += deterministic_calc(current);
-	}
-
-	printf("%d", counter);
-	printf(" primes in ");
-	time = clock() - time;
-	if(((double)time)/CLOCKS_PER_SEC < 10.0){printf(" ");}
-	if(((double)time)/CLOCKS_PER_SEC < 100.0){printf(" ");}
-	printf("%lf\n", ((double)time)/CLOCKS_PER_SEC);
+	clock_t end = clock();
+	printf(" in %lf\n", ((double)(end - start))/CLOCKS_PER_SEC);
 }
 //==================================================+ MILLER - RABIN  +==================================================
-unsigned int miller_rabin_calc(unsigned long a, unsigned long r,unsigned long d,unsigned long current){
-	unsigned int j, x; 
-	x = modulo(a,d,current);
-	if(x == 1||x == current - 1){		return 1;}
-	for(j = 0;j < r-1 && r > 1;j++){
+static unsigned long miller_rabin_calc(unsigned long a, unsigned long r, unsigned long d, unsigned long current)
+{
+	unsigned long x = modulo(a,d,current);
+	if (x == 1 || (x == current - 1))
+		return 1;
+	for (unsigned long j = 0;j < r-1 && r > 1;j++) {
 		x = modulo(x, 2, current);
-		if (x == current - 1){	return 1;}
+		if (x == current - 1)
+			return 1;
 	}
 	return 0;
 }
-//-----------------------------------------------------------------------------------------------------------------------
-void miller_rabin(unsigned int current, unsigned int range){
-	clock_t time = clock();
-	unsigned int counter = 0;
-	unsigned int i;
-	unsigned int r, d;
 
-	printf("Miller-Rabin  algorithm: Found ");
+static void miller_rabin(unsigned long current, unsigned long range)
+{
+	clock_t start = clock();
+	unsigned long counter = 0;
 
-	if(current == 1){current++;}
-	if(current == 2){current++;	counter++;}
-	if(current == 3){current += 2;	counter++;}
+	for (; current < range; current++) {
+		if (current == 1)
+			continue;
 
-	if(current % 2 == 0 && current + 1 <= range){current++;}
+		if (current == 2)
+			counter++;
+		if (current % 2 == 0)
+			continue;
 
-	for(;(current + 1) % 6 != 0 && current + 2 <= range;current += 2){	//"current" normalizer to the expected
-		if(current % 3 == 0){continue;}					//values {5,11,17,23,29,..... (6*n)-1}
+		if (current == 3)
+			counter++;
+		if (current % 3 == 0)
+			continue;
+		
+		if (current == 7 || current == 61) {
+			counter ++;
+			continue;
+		}
+		unsigned long r = 0;
+		for (r = 0;((current - 1) >> r) % 2 == 0;r++) {}
 
-		if(current == 7 || current == 61){counter ++; continue;}
-		for(r = 0;((current - 1) >> r) % 2 == 0;r++){}
-		d = ((current - 1) >> r);
-		counter += (miller_rabin_calc(2, r, d, current)
-			 && miller_rabin_calc(7, r, d, current)
-			&& miller_rabin_calc(61, r, d, current));
+		unsigned long d = ((current - 1) >> r);
+		counter += (
+			miller_rabin_calc(2, r, d, current)
+			&& miller_rabin_calc(7, r, d, current)
+			&& miller_rabin_calc(61, r, d, current)
+		);
 	}
-	for(; current + 2  <= range; current += 4){				//{2,4}step prime scanner
+	printf("Miller-Rabin  algorithm: Found %d primes", counter);
 
-		if(current == 7 || current == 61){counter ++; continue;}
-		for(r = 0;((current - 1) >> r) % 2 == 0;r++){}
-		d = ((current - 1) >> r);
-		counter += (miller_rabin_calc(2, r, d, current)
-			 && miller_rabin_calc(7, r, d, current)
-			&& miller_rabin_calc(61, r, d, current));
-
-		current += 2;
-
-		if(current == 7 || current == 61){counter ++; continue;}
-		for(r = 0;((current - 1) >> r) % 2 == 0;r++){}
-		d = ((current - 1) >> r);
-		counter += (miller_rabin_calc(2, r, d, current)
-			 && miller_rabin_calc(7, r, d, current)
-			&& miller_rabin_calc(61, r, d, current));
-	}
-	if(current < range){
-		if(current == 7 || current == 61){counter ++; current += 2;}
-		for(r = 0;((current - 1) >> r) % 2 == 0;r++){}
-		d = ((current - 1) >> r);
-		counter += (miller_rabin_calc(2, r, d, current)
-			 && miller_rabin_calc(7, r, d, current)
-			&& miller_rabin_calc(61, r, d, current));
-	}
-	printf("%d", counter);
-	printf(" primes in ");
-	time = clock() - time;
-	if(((double)time)/CLOCKS_PER_SEC < 10.0){printf(" ");}
-	if(((double)time)/CLOCKS_PER_SEC < 100.0){printf(" ");}
-	printf("%lf\n", ((double)time)/CLOCKS_PER_SEC);
+	clock_t end = clock();
+	printf(" in %lf\n", ((double)(end - start))/CLOCKS_PER_SEC);
 }
 
 //=======================================================+ MAIN  +=======================================================
-int main(){
+int main()
+{
 	printf("\n");
-	if(MAXIMUM > UINT_MAX){printf("Houston, we've had a problem here\n");return 0;}
+	if (MAXIMUM > ROOF) {
+		printf("MAXIMUM should be less than %ld\n", ROOF);
+		return 0;
+	}
+	if (MINIMUM > MAXIMUM) {
+		printf("MINIMUM should be less than MAXIMUM\n");
+		return 0;
+	}
 
-	printf("Checking range [%ld,%ld] for primes\n", (unsigned long)MINIMUM, (unsigned long)MAXIMUM);
+	printf("Checking range [%ld,%ld] for primes:\n", MINIMUM, MAXIMUM);
 
 	deterministic(MINIMUM, MAXIMUM);
 	miller_rabin(MINIMUM, MAXIMUM);
@@ -143,4 +134,3 @@ int main(){
 	printf("\n");
 	return 0;
 }
-
